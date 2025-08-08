c3f4ed30acfd0fdb8cb6881c80cec55f9c28f86a
Nicolas-Bolouri <nicolasbolouri@gmail.com>
2025-08-08 18:46:03 -0400

    tweak download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -6,4 +6,12 @@ curl -L -o clasp_data.zip https://archive.org/compress/clasp_data
 echo "Unzipping..."
 unzip clasp_data.zip
 
+echo "Cleaning up..."
+rm -f clasp_data_archive.torrent
+rm -f clasp_data_archive.zip
+rm -f clasp_data_files.xml
+rm -f clasp_data_meta.sqlite
+rm -f clasp_data_meta.xml
+rm -f clasp_data.zip
+
 echo "Done. Data available in $(pwd)/clasp_data"
